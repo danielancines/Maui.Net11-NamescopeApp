8c0af78f9772f2d0217969f07d968d38dd633323
Daniel Ancines <daniel.ancines@gmail.com>
2026-08-17 20:17:20 -0300

    Add project files.

---
+++ Maui.Net11-NamescopeApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.Net11_NamescopeApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
